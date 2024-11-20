faa535dd8160f3ef177455a44843722fc34f5350
Zeeker13 <ashenprageeth1@gmail.com>
2024-11-20 21:29:52 +0530

    added game pannel

--- client/src/components/pages/Gameplay.tsx
+++ client/src/components/pages/Gameplay.tsx
@@ -18,15 +18,32 @@ function Info() {
               <img src="" alt="" />
               <img className="" src="\src\assets\image\score.png" alt="Score" />
             </div>
-          </div>
-          <div className="absolute w-full h-full flex items-center justify-center z-10 pt-52">
-            <img
-              className="max-w-full max-h-full object-contain pb-[300px]"
-              src="\src\assets\image\highestscorewood.png"
-              alt="Info Instructions"
-            />
             
           </div>
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center">
+            <img className="justify-center " src="\src\assets\image\highestscorewood.png" alt="" />
+          </div>
+          
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center mt-80 gap-10  ">
+            {[...Array(10).keys()].map((num) => (
+              <div
+                key={num}
+                className=""
+              >
+                <img src="\src\assets\image\9.png" alt="" />
+                <div className=" h-full flex items-center justify-center">
+                  {num}
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="absolute bottom-0 pb-10 left-[90rem]">
+            <img src="\src\assets\image\submit.png" alt="" />
+          </div>
+
+
+          
           
 
           <div className="top-3/4 relative pl-24 bottom: -614px; bottom-[-614px]">
@@ -40,11 +57,16 @@ function Info() {
           </div>
         </div>
       </div>
-      <img
-        src="\src\assets\image\infopage.jpg"
-        alt="Background"
-        className=" w-full h-full object-cover"
-      />
+      <video
+        className="w-full h-full object-cover"
+        autoPlay
+        muted
+        loop
+        playsInline
+        controlsList="nodownload"
+      >
+        <source src="\src\assets\video\jungle.mp4" type="video/mp4" />
+      </video>
     </div>
   );
 }
