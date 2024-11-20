067c66cbaf16af48938e253750eac6ee7dc3baf7
Zeeker13 <ashenprageeth1@gmail.com>
2024-11-20 17:16:40 +0530

    added lives game play

--- client/src/components/pages/Gameplay.tsx
+++ client/src/components/pages/Gameplay.tsx
@@ -2,14 +2,32 @@ import { Link } from "react-router-dom";
 function Info() {
   return (
     <div className="w-screen h-screen relative overflow-hidden">
+       
       <div className="absolute w-full h-full">
-        <div className="absolute z-40 ">
-          <div>
-            <img className="p-10" src="\src\assets\image\lives.png" alt="" />
+        <div className="">
+          <div className="relative display flex justify-between">
+            <div className="absolute pl-36 pt-20">
+              <img
+                className=""
+                src="\src\assets\image\lives.png"
+                alt="Lives"
+              />
+              
+              <img className="pt-4" src="\src\assets\image\livelevels.png" alt="" />
+         
+            </div>
+            <div className="absolute top-0 right-0 p-20">
+              <img src="" alt="" />
+              <img
+                className=""
+                src="\src\assets\image\score.png"
+                alt="Score"
+              />
+            </div>
           </div>
-          <div className="pl-10 ">
-            <img src="\src\assets\image\livelevels.png" alt="" />
-          </div>
+
+         
+
           <div className="top-3/4 relative pl-24 bottom: -614px; bottom-[-614px]">
             <Link to="/play">
               <img
@@ -20,12 +38,13 @@ function Info() {
             </Link>
           </div>
         </div>
-        <img
+     
+      </div>
+      <img
           src="\src\assets\image\infopage.jpg"
           alt="Background"
-          className="w-full h-full object-cover"
+          className= " w-full h-full object-cover"
         />
-      </div>
 
       <div className="absolute w-full h-full flex items-center justify-center z-10 pt-52">
         <img
@@ -34,6 +53,10 @@ function Info() {
           alt="Info Instructions"
         />
       </div>
+      {/* <div className="flex absolute flex items-center justify-center space-x-10">
+        <img src="\src\assets\image\9.png" alt="" />
+
+      </div> */}
     </div>
   );
 }
